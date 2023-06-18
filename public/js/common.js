7a0c9e85d7a292d442a54782cbc3d0e5fb381781
annanay aggarwal <anni.agg2003@gmail.com>
2023-06-18 17:16:20 +0530

    post container

--- public/js/common.js
+++ public/js/common.js
@@ -14,13 +14,44 @@ $("#posttextarea").keyup((event)=>{
     submitbutton.prop("disabled",false)
 })
 
-$('#submitpostbutton').click(()=>{
+$('#submitpostbutton').click((event)=>{
     var button = $(event.target);
     var textbox = $('#posttextarea')
     var data = {
         content : textbox.val()
     }
-    $.post("/api/posts",data,(postdata,status,xhr)=>{
-        alert(postdata)
+    $.post("/api/posts",data,(postdata)=>{
+        
+        var html = craeteposthtml(postdata);
+        $(".postcontainer").prepend(html);             // prepend added to the top while appned at last
+        textbox.val("");
+        button.prop("disabled",true)
+
     })
 })
+
+function craeteposthtml(postdata){
+    var postedby = postdata.postedby;
+    var displayname =postedby.firstname+" "+ postedby.lastname;
+    var timestamp = "to do later"
+
+    return `<div class = 'post'>
+                <div class = 'maincontentcontainer'>
+                    <div class ="userimageconatiner">
+                        <img src = '${postedby.profilePic}' width = 50px >
+                    </div>
+                    <div class ="postcontentconatiner">
+                        <div class = "header">
+                            <a href='/profile/${postedby.username}'>${displayname}</a>
+                            <span class="username">${postedby.username}</span>
+                            <span class="username">${timestamp}</span>
+                        </div>
+                        <div class = "postbody">
+                            <span>${postdata.content}</span>
+                        </div>
+                        <div class = "postfooter">
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+}
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,6 +3,7 @@ const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser")
 const user = require("../../schemas/user");
+const post = require("../../schemas/postschema");
 
 app.use(bodyParser.urlencoded({extended:false}));
 
@@ -12,10 +13,22 @@ router.get("/", (req,res,next)=>{
 
 router.post("/", async(req,res,next)=>{
     if(!req.body.content){
-        console.log("content param not send with request")
+        console.log("content params not send with request")
         return res.sendStatus(400)
     }
-    res.status(200).send("workde")
-})
+    var postdata ={
+        content: req.body.content,
+        postedby:req.session.user
+    }
+    post.create(postdata)
+    .then(async(newpost)=>{
+        newpost = await user.populate(newpost,{path:"postedby "})
 
+        res.status(201).send(newpost)
+    })
+    .catch((err)=>{
+        console.log(err)
+        res.status(200).send("bad request")
+    })
+})
 module.exports = router
